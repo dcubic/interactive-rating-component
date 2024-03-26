3f7df0562fbdf76d1b1e8b5623c8052bcbbe6735
unknown <dominikcubic@gmail.com>
2024-03-26 15:46:12 -0700

    :construction: working locally; need to ensure that it works on github

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import SelectionWindow from './components/SelectionWindow/SelectionWindow'
 import ThankYouWindow from './components/ThankYouWindow/ThankYouWindow'
 
 function App() {
+  const [ratingState, setRatingState] = useState(undefined);
+  const [hasSubmittedState, setHasSubmittedState] = useState(false);
+  const ratingValues = [1, 2, 3, 4, 5];
+
   return (
     <body>
-      <SelectionWindow />
-      <ThankYouWindow />
+      {hasSubmittedState ? <></> : <SelectionWindow ratingValues = {ratingValues} ratingState={ratingState} setRatingState={setRatingState} setHasSubmittedState={setHasSubmittedState}/>}
+      {!hasSubmittedState ? <></> : <ThankYouWindow ratingState={ratingState} />}
     </body>
   )
 }
--- src/components/ThankYouWindow/ThankYouWindow.jsx
+++ src/components/ThankYouWindow/ThankYouWindow.jsx
@@ -1,22 +1,16 @@
-import "./ThankYouWindow.css"
-import starIcon from "../../assets/icon-star.svg"
+import styles from "./ThankYouWindow.module.css"
+import thankYouIllustration from "../../assets/illustration-thank-you.svg"
 
-export default function ThankYouWindow() {
-    
-    const ratingValues = [1, 2, 3, 4, 5];
-
+export default function ThankYouWindow(props) {
     return (
-        <main>
-            <div className="image-container">
-                <img src={starIcon}></img>
+        <main className={styles['background-panel']}>
+            <img src={thankYouIllustration} className={styles.imageStyle}></img>
+            <div className={styles.resultContainer}>
+                <p className={`${styles.noMargin} ${styles.result}`}>You selected {props.ratingState} out of 5</p>
             </div>
-            <h1>How did we do?</h1>
-            <p>Please let us know how we did with your support request. All feedback is appreciated 
-  to help us improve our offering!</p>
-            <div className="button-container">
-                {ratingValues.map(rating => (<button key={rating} className="selection-button">{rating}</button>))}
-            </div>
-            <button className="submit-button">Submit</button>
+            <h1 className={`${styles.noMargin} ${styles.header}`}>Thank you!</h1>
+            <p className={`${styles.noMargin} ${styles.appreciationMessage}`}>We appreciate you taking the time to give a rating. If you ever need more support, 
+don’t hesitate to get in touch!</p>
         </main>
     )
 }
